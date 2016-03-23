c1b3e7c5975a30c5b41839ddf3c2644583f53e38
shayoo <shayan.anwar93@gmail.com>
2016-03-23 12:34:05 +0500

    cdn and port 5000

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening on *:5000');
 });
